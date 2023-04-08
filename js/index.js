ace9e969a31f5dec983b5a2970ed560a6d28c6cb
CesarAlonzo <130248164+CesarAlonzo@users.noreply.github.com>
2023-04-08 13:15:23 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('Yay Ahora somos novios!!!!😊💞');
+    alert('Mi corazón te pertenece 🤎🥺');
+    location.href = 'https://www.youtube.com/watch?v=Fo5ELAwalfA';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  const partner = prompt('🥰 ¿Cuál es su nombre?🥰');
+  heroTitle.innerHTML += partner + ' 🤎';
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
